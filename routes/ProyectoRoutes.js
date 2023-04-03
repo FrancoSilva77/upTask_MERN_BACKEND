59d0b2356277032371f3c704e483eeef16dd3e01
Franco Silva <franco_tigres18@hotmail.com>
2023-04-03 17:57:15 -0600

    Controller y Routing Proyecto add

---
+++ routes/ProyectoRoutes.js
@@ -0,0 +1,30 @@
+import express from "express";
+import {
+  obtenerProyectos,
+  nuevoProyecto,
+  obtenerProyecto,
+  editarProyecto,
+  eliminarProyecto,
+  agregarColaborador,
+  eliminarColoborador,
+  obtenerTareas,
+} from "../controllers/proyectoController.js";
+
+import checkAuth from "../middleware/checkAuth.js";
+
+const router = express.Router();
+
+router.get("/", checkAuth, obtenerProyectos);
+router.post("/", checkAuth, nuevoProyecto);
+
+router
+  .route("/:id")
+  .get(checkAuth, obtenerProyecto)
+  .put(checkAuth, editarProyecto)
+  .delete(checkAuth, eliminarProyecto);
+
+router.get("/tareas/:id", checkAuth, obtenerTareas);
+router.post('/agregar-colaborador/:id', checkAuth, agregarColaborador)
+router.post('/eliminar-colaborador/:id', checkAuth, eliminarColaborador)
+
+export default router;
