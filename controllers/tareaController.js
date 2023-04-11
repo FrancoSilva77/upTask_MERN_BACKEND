13be610f8561cac42f5de5e46e4685f7496a6f7f
Franco Silva <franco_tigres18@hotmail.com>
2023-04-11 08:45:47 -0600

    Funcionalidad Actualizar Tarea add

--- controllers/tareaController.js
+++ controllers/tareaController.js
@@ -41,7 +41,33 @@ const obtenerTarea = async (req, res) => {
   }
 };
 
-const actualizarTarea = async (req, res) => {};
+const actualizarTarea = async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const tarea = await Tarea.findById(id).populate("proyecto");
+    if (tarea.proyecto.creador.toString() !== req.usuario._id.toString()) {
+      const error = new Error("Acción no Válida");
+      return res.status(403).json({ msg: error.message });
+    }
+
+    // Actualizar
+    tarea.nombre = req.body.nombre || tarea.nombre;
+    tarea.descripcion = req.body.descripcion || tarea.descripcion;
+    tarea.prioridad = req.body.prioridad || tarea.prioridad;
+    tarea.fechaEntrega = req.body.fechaEntrega || tarea.fechaEntrega;
+
+    try {
+      const tareaAlmacenada = await tarea.save();
+      res.json(tareaAlmacenada);
+    } catch (error) {
+      console.log(error);
+    }
+  } catch (error) {
+    error = new Error("La tarea no existe");
+    return res.status(404).json({ msg: error.message });
+  }
+};
 
 const eliminarTarea = async (req, res) => {};
 
